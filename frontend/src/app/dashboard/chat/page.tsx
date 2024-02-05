9b5ff7a22164d7d1fc2ada293371d7776bada59d
aidandh <aidanhert@hotmail.com>
2024-02-05 14:47:04 -0800

    added links to chat pages

--- frontend/src/app/dashboard/chat/page.tsx
+++ frontend/src/app/dashboard/chat/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { TeamRole } from "@/types";
 
 export default function ChatPage() {
     return (
         <div>
             <h2>Chat Page</h2>
+            <Link href="/dashboard">Back to dashboard</Link>
             <p>Welcome to the chat page!</p>
-            <Link href="/dashboard">Go to dashboard</Link>
+            <div>
+                
+            </div>
         </div>
     );
 };
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Dashboard() {
     return (
         <div>
             <h2>Dashboard</h2>
             <p>Welcome to your dashboard.</p>
+            <Link href="/dashboard/chat">Go to chat</Link>
         </div>
     );
 }
