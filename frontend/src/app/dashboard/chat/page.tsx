c7a49c1abeaca86b4db8f8c504d7618d28e0fe9b
Aidan Hert <aidanhert@hotmail.com>
2024-02-09 20:56:16 -0800

    capitalizes role name in chat selection

--- frontend/src/app/dashboard/chat/page.tsx
+++ frontend/src/app/dashboard/chat/page.tsx
@@ -6,25 +6,30 @@ import styles from "./ChatPage.module.css";
 import { User } from "@/types";
 
 export default async function ChatPage() {
+    const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+    
     const user = await validateUser();
     if (!user) {
         redirect("/login");
     }
 
     let teamUsers: User[] = [];
-    if (user.team)
+    let teamName;
+    if (user.team) {
         teamUsers = await getTeamUsers(user.team.id);
+        teamName = user.team.name;
+    }
 
     return (
         <div>
             <p>Welcome to the chat page!</p>
             { user.team ?
                 <ul>
-                    <Link href="/dashboard/chat/team"><li className={styles.chatList}>Team</li></Link>
+                    <Link href="/dashboard/chat/team"><li className={styles.chatList}>{teamName}</li></Link>
                     {teamUsers.map((teamUser) => (
                         <li className={styles.chatList} key={teamUser.id}>
                             <Link href={`/dashboard/chat/${teamUser.username}`}>
-                                {teamUser.teamRole} ({teamUser.username})
+                                {capitalize(teamUser.teamRole)} ({teamUser.username})
                             </Link>
                         </li>
                     ))}
