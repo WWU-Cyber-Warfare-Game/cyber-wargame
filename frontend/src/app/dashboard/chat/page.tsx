044c26065644423a6382347d7309fabf6ceb3dfc
Aidan Hert <aidanhert@hotmail.com>
2024-02-09 20:42:17 -0800

    added check to see if user not on team

--- frontend/src/app/dashboard/chat/page.tsx
+++ frontend/src/app/dashboard/chat/page.tsx
@@ -3,6 +3,7 @@ import { getTeamUsers, validateUser } from "@/actions";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import styles from "./ChatPage.module.css";
+import { User } from "@/types";
 
 export default async function ChatPage() {
     const user = await validateUser();
@@ -10,21 +11,27 @@ export default async function ChatPage() {
         redirect("/login");
     }
 
-    const teamUsers = await getTeamUsers(user.team.id);
-    
+    let teamUsers: User[] = [];
+    if (user.team)
+        teamUsers = await getTeamUsers(user.team.id);
+
     return (
         <div>
             <p>Welcome to the chat page!</p>
-            <ul>
-                <Link href="/dashboard/chat/team"><li className={styles.chatList}>Team</li></Link>
-                {teamUsers.map((teamUser) => (
-                    <li className={styles.chatList} key={teamUser.id}>
-                        <Link href={`/dashboard/chat/${teamUser.username}`}>
-                            {teamUser.teamRole} ({teamUser.username})
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            { user.team ?
+                <ul>
+                    <Link href="/dashboard/chat/team"><li className={styles.chatList}>Team</li></Link>
+                    {teamUsers.map((teamUser) => (
+                        <li className={styles.chatList} key={teamUser.id}>
+                            <Link href={`/dashboard/chat/${teamUser.username}`}>
+                                {teamUser.teamRole} ({teamUser.username})
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                :
+                <p>You are not currently on a team.</p>
+            }
         </div>
     );
 };
