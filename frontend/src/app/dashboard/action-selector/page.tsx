730fdea35d69018b2cf677cd855606d7f20febec
jathaw00 <mzeandcrky@gmail.com>
2024-03-08 21:58:43 -0800

    Action Selector Page commit with component funct. 	new file:   frontend/src/app/dashboard/action-selector/page.tsx 	new file:   frontend/src/components/ActionSelectorFrame/indes.tsx

---
+++ frontend/src/app/dashboard/action-selector/page.tsx
@@ -0,0 +1,18 @@
+
+import Link from "next/link";
+import { ActionSelectorFrame } from "@/components/ActionSelectorFrame/indes";
+
+export default function ActionSelectorPage() {
+  
+  return (
+    <div>
+      <h2>Action Selector</h2>
+      <Link href="/dashboard/chat">Go to Chat</Link>
+      <br />
+      <Link href="/dashboard">Go to Dashboard</Link>
+      <br />
+      <Link href="/dashboard/action-log">Go to Action Log</Link>
+      <ActionSelectorFrame>
+   </div>   
+  );
+}
