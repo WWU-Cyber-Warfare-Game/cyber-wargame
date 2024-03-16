34aa011aae0884f0bbacb53a3f54229fd15d245e
Aidan Hert <aidanhert@hotmail.com>
2024-03-15 17:25:03 -0700

    created Timer component

---
+++ frontend/src/components/Timer.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+interface TimerProps {
+    readonly time: Date;
+}
+
+export default function Timer({ time }: Readonly<TimerProps>) {
+    const [timeLeft, setTimeLeft] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+    // Update the time left every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeLeft(time.getTime() - Date.now());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [time]);
+
+    // update the hours, minutes, and seconds
+    useEffect(() => {
+        setSeconds(Math.floor((timeLeft / 1000) % 60));
+        setMinutes(Math.floor((timeLeft / 1000 / 60) % 60));
+        setHours(Math.floor(timeLeft / 1000 / 60 / 60));
+    }, [timeLeft]);
+
+    return <p>{hours}h {minutes}m {seconds}s</p>
+}
