1b92c02085561a2c6e9a4ba56f5d31f6ae9b36fa
Sol-Aestuans <aidansol@outlook.com>
2024-04-21 15:38:00 -0700

    animated node

--- frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
+++ frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
@@ -8,7 +8,7 @@ export default function NetworkGraph() {
         {id: '1', position: {x: 0, y: 0}, data: {label: 'node 1'}},
         {id: '2', position: {x: 0, y: 100}, data: {label: 'node 2'}}
     ];
-    const initialEdges = [{id: 'edge 1-2', source: '1', target: '2'}];
+    const initialEdges = [{id: 'edge 1-2', source: '1', target: '2', animated:true}];
 
     return (
         // must be inside div
