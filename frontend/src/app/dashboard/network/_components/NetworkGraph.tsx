6a1fd40910a6364ffeb7b27221872952e3196aa3
Sol-Aestuans <aidansol@outlook.com>
2024-04-21 17:03:49 -0700

    node-population branch created

--- frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
+++ frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
@@ -1,19 +1,46 @@
-import React from "react";
-import ReactFlow, { EdgeLabelRenderer } from "reactflow";
+import React, { useEffect, useState } from "react";
+import ReactFlow from "reactflow";
 import 'reactflow/dist/style.css';
+import axios from "axios";
 
+// fetches the data from strapi
+const populateNetwork = async () => {
+    try {
+      // Make API request to fetch nodes
+      const nodesResponse = await axios.get('STRAPI_NODES_API_ENDPOINT');
+      const fetchedNodes = nodesResponse.data;
+  
+      // Make API request to fetch edges
+      const edgesResponse = await axios.get('STRAPI_EDGES_API_ENDPOINT');
+      const fetchedEdges = edgesResponse.data;
+  
+      return { nodes: fetchedNodes, edges: fetchedEdges };
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return { nodes: [], edges: [] };
+    }
+  };
+
+// creates the networkGraph component
 export default function NetworkGraph() {
+    const [nodes, setNodes] = useState([]);
+    const [edges, setEdges] = useState([]);
 
-    const initialNodes = [
-        {id: '1', position: {x: 0, y: 0}, data: {label: 'node 1'}},
-        {id: '2', position: {x: 0, y: 100}, data: {label: 'node 2'}}
-    ];
-    const initialEdges = [{id: 'edge 1-2', source: '1', target: '2', animated:true}];
+    // fetch the network from strapi
+    useEffect(() => {
+        const fetchData = async () => {
+          const { nodes, edges } = await populateNetwork();
+          setNodes(nodes);
+          setEdges(edges);
+        };
+    
+        fetchData();
+    }, []);
 
     return (
         // must be inside div
         <div style={{ width: '100%', height: '500px', border: 'solid 1px red', }}>
-            <ReactFlow nodes={initialNodes} edges={initialEdges} fitView/>
+            <ReactFlow nodes={nodes} edges={edges} fitView/>
         </div>
     )
 };
