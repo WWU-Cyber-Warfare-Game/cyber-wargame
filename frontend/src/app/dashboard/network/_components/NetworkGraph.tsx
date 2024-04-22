dac965d0db8c7d9f35bd327de93490f5c5fe4d88
Sol-Aestuans <aidansol@outlook.com>
2024-04-21 21:53:15 -0700

    network graph fix

--- frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
+++ frontend/src/app/dashboard/network/_components/NetworkGraph.tsx
@@ -1,25 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactFlow from "reactflow";
 import 'reactflow/dist/style.css';
-import axios from "axios";
-
-// fetches the data from strapi
-const populateNetwork = async () => {
-    try {
-      // Make API request to fetch nodes
-      const nodesResponse = await axios.get('STRAPI_NODES_API_ENDPOINT');
-      const fetchedNodes = nodesResponse.data;
-  
-      // Make API request to fetch edges
-      const edgesResponse = await axios.get('STRAPI_EDGES_API_ENDPOINT');
-      const fetchedEdges = edgesResponse.data;
-  
-      return { nodes: fetchedNodes, edges: fetchedEdges };
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return { nodes: [], edges: [] };
-    }
-  };
+// import { populateNetwork } from "@/actions";
 
 // creates the networkGraph component
 export default function NetworkGraph() {
@@ -27,15 +9,15 @@ export default function NetworkGraph() {
     const [edges, setEdges] = useState([]);
 
     // fetch the network from strapi
-    useEffect(() => {
-        const fetchData = async () => {
-          const { nodes, edges } = await populateNetwork();
-          setNodes(nodes);
-          setEdges(edges);
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //       const { nodes, edges } = await populateNetwork();
+    //     //   setNodes(nodes);
+    //     //   setEdges(edges);
+    //     };
     
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
     return (
         // must be inside div
