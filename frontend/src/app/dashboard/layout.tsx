3f1a66f7d0db7d481eeb64d270f587c1ec4c7482
Aidan Hert <aidanhert@hotmail.com>
2024-05-09 00:19:23 -0700

    remove test print

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -3,7 +3,6 @@ import { getGameState } from "@/actions";
 export default async function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     try {
         const gameState = await getGameState();
-        console.log(gameState);
         if (!gameState.gameRunning) {
             return <div>Game has not started yet.</div>;
         } else {
