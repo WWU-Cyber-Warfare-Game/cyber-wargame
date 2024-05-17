aab023237fd3de6ebd565c3e7f83c3cf47e6017a
jathaw00 <mzeandcrky@gmail.com>
2024-05-16 17:54:18 -0700

    first css push

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import NetworkFrame from "./network/NetworkFrame";
-
+import "./dashboard.css";
 /**
  * The dashboard page for the application. Displays a welcome message and a link to the chat page.
  * @returns 
  */
 export default function Dashboard() {
-    return (
-        <div>
+    return (        
+        <div className="dashNav">
             <h2>Dashboard</h2>
             <p>Welcome to your dashboard.</p>
             <Link href="/dashboard/chat"><p>Go to chat</p></Link>
